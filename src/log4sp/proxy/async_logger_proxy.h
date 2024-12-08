97c179e4f9325c3cff76fc07fe0a2fb29d86b5f6
F1F88 <0xf1f88@gmail.com>
2024-12-08 20:42:37 +0800

    refactor: Solve the defects of spdlog::logger through the proxy design pattern
    * Solve the concurrent security issues of asynchronous logger
    * Implement custom error handler
    
    - 通过代理设计模式解决 spdlog::logger 存在的缺陷
    * 解决异步 logger 的并发安全问题
    * 实现自定义错误处理器

---
+++ src/log4sp/proxy/async_logger_proxy.h
@@ -0,0 +1,51 @@
+#ifndef _LOG4SP_PROXY_ASYNC_LOGGER_PROXY_H_
+#define _LOG4SP_PROXY_ASYNC_LOGGER_PROXY_H_
+
+#include "spdlog/async_logger.h"
+#include "spdlog/sinks/dist_sink.h"
+
+#include "log4sp/proxy/logger_proxy.h"
+
+
+namespace log4sp {
+
+/**
+ * 为了实现这个类，我们修改了 spdlog::async_logger 源码
+ */
+class async_logger_proxy final : public spdlog::async_logger,
+                                 public logger_proxy {
+    friend class spdlog::details::thread_pool;
+
+public:
+    async_logger_proxy(std::string name,
+                       std::shared_ptr<spdlog::sinks::dist_sink_mt> sink,
+                       std::weak_ptr<spdlog::details::thread_pool> tp,
+                       spdlog::async_overflow_policy policy = spdlog::async_overflow_policy::block)
+        : spdlog::logger(std::move(name), std::move(sink)),
+          spdlog::async_logger(std::move(name), std::move(sink), std::move(tp), policy),
+          log4sp::logger_proxy(std::move(name), std::move(sink)) {}
+
+    ~async_logger_proxy() override;
+
+    void add_sink(spdlog::sink_ptr sink) override;
+
+    void remove_sink(spdlog::sink_ptr sink) override;
+
+    void set_error_forward(IChangeableForward *forward) override;
+
+    void error_handler(spdlog::source_loc loc, const std::string &msg) override;
+
+protected:
+    void sink_it_(const spdlog::details::log_msg &msg) override;
+    void flush_() override;
+    void backend_sink_it_(const spdlog::details::log_msg &incoming_log_msg);
+    void backend_flush_();
+
+private:
+    std::mutex error_mutex_;
+};
+
+
+}       // namespace log4sp
+#include "log4sp/proxy/async_logger_proxy-inl.h"
+#endif  // _LOG4SP_PROXY_ASYNC_LOGGER_PROXY_H_
---
+++ src/log4sp/proxy/logger_proxy.h
@@ -0,0 +1,61 @@
+#ifndef _LOG4SP_PROXY_LOGGER_PROXY_H_
+#define _LOG4SP_PROXY_LOGGER_PROXY_H_
+
+#include "spdlog/logger.h"
+
+#include "extension.h"
+
+
+namespace log4sp {
+
+/**
+ * 原版 spdlog::logger 存在的问题
+ *   1. sinks 和 err_handler_ 是线程不安全的
+ *   2. err_handler_ 不是虚函数，不能使用继承和多态
+ *   3. err_handler_ 的权限是 protected，不能被外部调用
+ *   4. async_logger 被 final 修饰，无法继承，重写可能还涉及线程池部分
+ *
+ * 方案：
+ *   1. 通过继承添加 add_sink 和 remove_sink 方法解决
+ *   2. 通过继承重写 sink_it_ 和 flush_ 捕获到异常时的处理方案（）
+ *   3. 通过继承添加 error_handler()
+ *   4. 修改源码，将 async_logger 设为了非 final 类
+ */
+class logger_proxy : virtual public spdlog::logger {
+public:
+    explicit logger_proxy(std::string name)
+        : spdlog::logger(std::move(name)), error_forward_(nullptr) {}
+
+    template <typename It>
+    logger_proxy(std::string name, It begin, It end)
+        : spdlog::logger(std::move(name), begin, end), error_forward_(nullptr) {}
+
+    logger_proxy(std::string name, spdlog::sink_ptr single_sink)
+        : spdlog::logger(std::move(name), std::move(single_sink)), error_forward_(nullptr) {}
+
+    logger_proxy(std::string name, spdlog::sinks_init_list sinks)
+        : spdlog::logger(std::move(name), std::move(sinks)), error_forward_(nullptr) {}
+
+    ~logger_proxy() override;
+
+    virtual void add_sink(spdlog::sink_ptr sink);
+
+    virtual void remove_sink(spdlog::sink_ptr sink);
+
+    virtual void set_error_forward(IChangeableForward *forward);
+
+    // sink_it_ 和 flush_ 遇到异常时将调用这个以替代 spdlog::logger 的 err_handler_
+    virtual void error_handler(spdlog::source_loc loc, const std::string &msg);
+
+protected:
+    IChangeableForward *error_forward_;     // 通过 set_error_forward() 赋值
+
+    // 重写以在捕获到异常时自定义处理方案
+    void sink_it_(const spdlog::details::log_msg &msg) override;
+    void flush_() override;
+};
+
+
+}       // namespace log4sp
+#include "log4sp/proxy/logger_proxy-inl.h"
+#endif  // _LOG4SP_PROXY_LOGGER_PROXY_H_
