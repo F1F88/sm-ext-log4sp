5a60f4b99e04cddaa4fc7b392a36d1ef7c570c22
F1F88 <0xf1f88@gmail.com>
2025-01-04 00:11:36 +0800

    fix: Possible crash caused by sink native

--- src/natives/sinks/rorating_file_sink.cpp
+++ src/natives/sinks/rorating_file_sink.cpp
@@ -22,50 +22,40 @@ static cell_t RotatingFileSink(IPluginContext *ctx, const cell_t *params)
     char path[PLATFORM_MAX_PATH];
     smutils->BuildPath(Path_Game, path, sizeof(path), "%s", file);
 
-    auto maxFileSize = static_cast<size_t>(params[2]);
-    if (maxFileSize <= 0)
-    {
-        ctx->ReportError("maxFileSize arg must be an integer greater than 0.");
-        return BAD_HANDLE;
-    }
-
-    auto maxFiles = static_cast<size_t>(params[3]);
-    if (maxFiles > 200000)
-    {
-        ctx->ReportError("maxFiles arg cannot exceed 200000.");
-        return BAD_HANDLE;
-    }
-
+    auto maxFileSize  = static_cast<size_t>(params[2]);
+    auto maxFiles     = static_cast<size_t>(params[3]);
     auto rotateOnOpen = static_cast<bool>(params[4]);
+    bool multiThread  = static_cast<bool>(params[5]);
+
+    spdlog::sink_ptr sink;
+    try
+    {
+        if (!multiThread)
+        {
+            sink = std::make_shared<spdlog::sinks::rotating_file_sink_st>(path, maxFileSize, maxFiles, rotateOnOpen);
+        }
+        else
+        {
+            sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(path, maxFileSize, maxFiles, rotateOnOpen);
+        }
+    }
+    catch(const std::exception &ex)
+    {
+        ctx->ReportError(ex.what());
+        return BAD_HANDLE;
+    }
 
     HandleSecurity security{nullptr, myself->GetIdentity()};
     HandleError error;
 
-    bool multiThread = static_cast<bool>(params[5]);
-    if (!multiThread)
+    auto handle = log4sp::sink_handler::instance().create_handle(sink, &security, nullptr, &error);
+    if (handle == BAD_HANDLE)
     {
-        auto sink   = std::make_shared<spdlog::sinks::rotating_file_sink_st>(path, maxFileSize, maxFiles, rotateOnOpen);
-        auto handle = log4sp::sink_handler::instance().create_handle(sink, &security, nullptr, &error);
-        if (handle == BAD_HANDLE)
-        {
-            ctx->ReportError("Allocation of sink handle failed. (err: %d)", handle, error);
-            return BAD_HANDLE;
-        }
-
-        return handle;
+        ctx->ReportError("Allocation of sink handle failed. (err: %d)", handle, error);
+        return BAD_HANDLE;
     }
-    else
-    {
-        auto sink   = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(path, maxFileSize, maxFiles, rotateOnOpen);
-        auto handle = log4sp::sink_handler::instance().create_handle(sink, &security, nullptr, &error);
-        if (handle == BAD_HANDLE)
-        {
-            ctx->ReportError("Allocation of multi thread sink handle failed. (err: %d)", handle, error);
-            return BAD_HANDLE;
-        }
 
-        return handle;
-    }
+    return handle;
 }
 
 /**
@@ -128,7 +118,14 @@ static cell_t RotatingFileSink_RotateNow(IPluginContext *ctx, const cell_t *para
         auto realSink = std::dynamic_pointer_cast<spdlog::sinks::rotating_file_sink_st>(sink);
         if (realSink != nullptr)
         {
-            realSink->rotate_now();
+            try
+            {
+                realSink->rotate_now();
+            }
+            catch(const std::exception &ex)
+            {
+                ctx->ReportError(ex.what());
+            }
             return 0;
         }
     }
@@ -137,7 +134,14 @@ static cell_t RotatingFileSink_RotateNow(IPluginContext *ctx, const cell_t *para
         auto realSink = std::dynamic_pointer_cast<spdlog::sinks::rotating_file_sink_mt>(sink);
         if (realSink != nullptr)
         {
-            realSink->rotate_now();
+            try
+            {
+                realSink->rotate_now();
+            }
+            catch(const std::exception &ex)
+            {
+                ctx->ReportError(ex.what());
+            }
             return 0;
         }
     }
--- src/natives/sinks/server_console_sink.cpp
+++ src/natives/sinks/server_console_sink.cpp
@@ -11,34 +11,37 @@
  */
 static cell_t ServerConsoleSink(IPluginContext *ctx, const cell_t *params)
 {
-    HandleSecurity security{ctx->GetIdentity(), myself->GetIdentity()};
+    bool multiThread = static_cast<bool>(params[1]);
+
+    spdlog::sink_ptr sink;
+    try
+    {
+        if (!multiThread)
+        {
+            sink = std::make_shared<spdlog::sinks::stdout_sink_st>();
+        }
+        else
+        {
+            sink = std::make_shared<spdlog::sinks::stdout_sink_mt>();
+        }
+    }
+    catch(const std::exception &ex)
+    {
+        ctx->ReportError(ex.what());
+        return BAD_HANDLE;
+    }
+
+    HandleSecurity security{nullptr, myself->GetIdentity()};
     HandleError error;
 
-    bool multiThread = static_cast<bool>(params[1]);
-    if (!multiThread)
+    Handle_t handle = log4sp::sink_handler::instance().create_handle(sink, &security, nullptr, &error);
+    if (handle == BAD_HANDLE)
     {
-        auto sink   = std::make_shared<spdlog::sinks::stdout_sink_st>();
-        auto handle = log4sp::sink_handler::instance().create_handle(sink, &security, nullptr, &error);
-        if (handle == BAD_HANDLE)
-        {
-            ctx->ReportError("Allocation of ServerConsoleSink handle failed. (err: %d)", handle, error);
-            return BAD_HANDLE;
-        }
-
-        return handle;
+        ctx->ReportError("Allocation of sink handle failed. (err: %d)", handle, error);
+        return BAD_HANDLE;
     }
-    else
-    {
-        auto sink   = std::make_shared<spdlog::sinks::stdout_sink_mt>();
-        auto handle = log4sp::sink_handler::instance().create_handle(sink, &security, nullptr, &error);
-        if (handle == BAD_HANDLE)
-        {
-            ctx->ReportError("Allocation of multi thread ServerConsoleSink handle failed. (err: %d)", handle, error);
-            return BAD_HANDLE;
-        }
 
-        return handle;
-    }
+    return handle;
 }
 
 const sp_nativeinfo_t ServerConsoleSinkNatives[] =
