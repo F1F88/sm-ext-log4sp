5022b47935d49133acd93c383cc14a45b6918bc1
F1F88 <0xf1f88@gmail.com>
2025-01-12 14:56:25 +0800

    Revert "feat: Support custom asynchronous logger"
    This reverts commit 5a05a42cc3e8d9adbae431541e899a7f23726470.

--- extern/spdlog/include/spdlog/details/thread_pool.h
+++ extern/spdlog/include/spdlog/details/thread_pool.h
@@ -14,12 +14,11 @@
 #include <vector>
 
 namespace spdlog {
+class async_logger;
 
 namespace details {
-//* Log4sp customization *//
-class backend_worker;
 
-using backend_worker_ptr = std::shared_ptr<backend_worker>;
+using async_logger_ptr = std::shared_ptr<spdlog::async_logger>;
 
 enum class async_msg_type { log, flush, terminate };
 
@@ -27,7 +26,7 @@ enum class async_msg_type { log, flush, terminate };
 // Movable only. should never be copied
 struct async_msg : log_msg_buffer {
     async_msg_type msg_type{async_msg_type::log};
-    backend_worker_ptr worker_ptr;
+    async_logger_ptr worker_ptr;
 
     async_msg() = default;
     ~async_msg() = default;
@@ -54,12 +53,12 @@ struct async_msg : log_msg_buffer {
 #endif
 
     // construct from log_msg with given type
-    async_msg(backend_worker_ptr &&worker, async_msg_type the_type, const details::log_msg &m)
+    async_msg(async_logger_ptr &&worker, async_msg_type the_type, const details::log_msg &m)
         : log_msg_buffer{m},
           msg_type{the_type},
           worker_ptr{std::move(worker)} {}
 
-    async_msg(backend_worker_ptr &&worker, async_msg_type the_type)
+    async_msg(async_logger_ptr &&worker, async_msg_type the_type)
         : log_msg_buffer{},
           msg_type{the_type},
           worker_ptr{std::move(worker)} {}
@@ -86,10 +85,10 @@ public:
     thread_pool(const thread_pool &) = delete;
     thread_pool &operator=(thread_pool &&) = delete;
 
-    void post_log(backend_worker_ptr &&worker_ptr,
+    void post_log(async_logger_ptr &&worker_ptr,
                   const details::log_msg &msg,
                   async_overflow_policy overflow_policy);
-    void post_flush(backend_worker_ptr &&worker_ptr, async_overflow_policy overflow_policy);
+    void post_flush(async_logger_ptr &&worker_ptr, async_overflow_policy overflow_policy);
     size_t overrun_counter();
     void reset_overrun_counter();
     size_t discard_counter();
