69f844ec29ca58acf67ce944e574962ccb803393
F1F88 <0xf1f88@gmail.com>
2025-01-22 18:51:25 +0800

    feat: Enhance Sink.Log function

--- src/natives/sinks/sink.cpp
+++ src/natives/sinks/sink.cpp
@@ -97,7 +97,7 @@ static cell_t ShouldLog(IPluginContext *ctx, const cell_t *params)
 }
 
 /**
- * public native void Log(const char[] name, LogLevel lvl, const char[] msg);
+ * public native void Log(const char[] name, LogLevel lvl, const char[] msg, const char[] file = NULL_STRING, int line = 0, const char[] func = NULL_STRING, int seconds[2] = {0, 0}, int nanoseconds[2] = {0, 0});
  */
 static cell_t Log(IPluginContext *ctx, const cell_t *params)
 {
@@ -113,17 +113,55 @@ static cell_t Log(IPluginContext *ctx, const cell_t *params)
         return 0;
     }
 
-    char *name;
+    char *name, *msg;
     ctx->LocalToString(params[2], &name);
+    ctx->LocalToString(params[4], &msg);
 
     auto lvl = log4sp::cell_to_level(params[3]);
 
-    char *msg;
-    ctx->LocalToString(params[4], &msg);
+    char *file, *func;
+    ctx->LocalToStringNULL(params[5], &file);
+    ctx->LocalToStringNULL(params[7], &func);
+
+    auto line = static_cast<int>(params[6]);
+
+    spdlog::source_loc loc{};
+    if (file && line > 0 && func)
+    {
+        loc = spdlog::source_loc{file, line, func};
+    }
+
+    cell_t *seconds, *nanoseconds;
+    ctx->LocalToPhysAddr(params[8], &seconds);
+    ctx->LocalToPhysAddr(params[9], &nanoseconds);
+
+    std::chrono::system_clock::time_point logTime{spdlog::details::os::now()};
+    if (nanoseconds[0] != 0 || nanoseconds[1] != 0)
+    {
+        logTime = std::chrono::system_clock::time_point{
+            std::chrono::duration_cast<std::chrono::system_clock::duration>(
+                std::chrono::nanoseconds{
+                    (static_cast<int64_t>(static_cast<uint32_t>(nanoseconds[1])) << 32) |
+                    static_cast<uint32_t>(nanoseconds[0])
+                }
+            )
+        };
+    }
+    else if (seconds[0] != 0 || seconds[1] != 0)
+    {
+        logTime = std::chrono::system_clock::time_point{
+            std::chrono::duration_cast<std::chrono::system_clock::duration>(
+                std::chrono::seconds{
+                    (static_cast<int64_t>(static_cast<uint32_t>(seconds[1])) << 32) |
+                    static_cast<uint32_t>(seconds[0])
+                }
+            )
+        };
+    }
 
     try
     {
-        sink->log(spdlog::details::log_msg(name, lvl, msg));
+        sink->log(spdlog::details::log_msg{logTime, loc, name, lvl, msg});
     }
     catch (const std::exception &ex)
     {
