12951e866071ed9725af3e6be2b1a6202e094cf6
F1F88 <0xf1f88@gmail.com>
2026-08-11 14:47:26 +0800

    Revert "feat(spdlog-linux): Add sink::to_pattern interface"
    This reverts commit 4602240bf39dc8f0fbc6fb8657773c2de1f13a2d.

--- extern/spdlog/include/spdlog/sinks/stdout_sinks.h
+++ extern/spdlog/include/spdlog/sinks/stdout_sinks.h
@@ -35,9 +35,6 @@ public:
 
     void set_formatter(std::unique_ptr<spdlog::formatter> sink_formatter) override;
 
-    //* @log4sp hack *//
-    [[nodiscard]] std::string to_pattern(const details::log_msg &log_msg) final override;
-
 protected:
     mutex_t &mutex_;
     FILE *file_;
