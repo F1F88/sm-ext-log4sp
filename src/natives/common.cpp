d4f7dff872706fb6e5406a983d8964b0b722e17a
F1F88 <0xf1f88@gmail.com>
2026-08-19 04:13:05 +0800

    feat: Changed `LogLevelToName`, `LogLevelToShortName`, and `NameToLogLevel` from **native** to pure **stock** implementations

--- src/natives/common.cpp
+++ src/natives/common.cpp
@@ -4,36 +4,6 @@
 #include "log4sp/common.h"
 
 
-static cell_t LogLevelToName(SourcePawn::IPluginContext *ctx, const cell_t *params) noexcept
-{
-    using spdlog::level::to_string_view;
-
-    auto lvl = Log4sp::NumToLvl(params[3]);
-    auto name = spdlog::level::to_string_view(lvl);
-
-    std::size_t bytes = 0;
-    CTX_STRING_TO_LOCAL_UTF8(params[1], params[2], name.data(), &bytes);
-    return static_cast<cell_t>(bytes);
-}
-
-static cell_t LogLevelToShortName(SourcePawn::IPluginContext *ctx, const cell_t *params) noexcept
-{
-    auto lvl = Log4sp::NumToLvl(params[3]);
-    auto name = spdlog::level::to_short_c_str(lvl);
-
-    std::size_t bytes = 0;
-    CTX_STRING_TO_LOCAL_UTF8(params[1], params[2], name, &bytes);
-    return static_cast<cell_t>(bytes);
-}
-
-static cell_t NameToLogLevel(SourcePawn::IPluginContext *ctx, const cell_t *params) noexcept
-{
-    char *name;
-    CTX_LOCAL_TO_STRING(params[1], &name);
-
-    return static_cast<cell_t>(spdlog::level::from_str(name));
-}
-
 static cell_t GetLog4spVersion(SourcePawn::IPluginContext *ctx, const cell_t *params) noexcept
 {
     constexpr int MAJOR = LOG4SP_V_MAJOR;
@@ -66,9 +36,6 @@ static cell_t GetLog4spVersion(SourcePawn::IPluginContext *ctx, const cell_t *pa
 
 const sp_nativeinfo_t CommonNatives[] =
 {
-    {"LogLevelToName",              LogLevelToName},
-    {"LogLevelToShortName",         LogLevelToShortName},
-    {"NameToLogLevel",              NameToLogLevel},
     {"GetLog4spVersion",            GetLog4spVersion},
 
     {nullptr,                       nullptr}
